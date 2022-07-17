1bb1316b717677c2099dbafbcb2bc5ce9cae7428
bruce.wang <www6v@126.com>
2022-07-17 12:32:00 +0800

    solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,74 @@
-public class Solution {
-    private long freezing = 0;
-    private long boiling = 100;
+import java.util.Scanner;
 
-    private long priviousTemperature;
-    private long currentTemperature;
+public class Solution {
+    public static final int freezingThresholds = 0;
+    private static final int boilingThresholds = 100;
+    public static final String FREEZING = "freezing";
+    public static final String BOILING = "boiling";
+    public static final String UNFREEZING = "unfreezing";
+    public static final String UNBOILING = "unboiling";
+
+
+    private double priviousTemperature;
+    private double currentTemperature;
 
     private String currentStatus;
 
-    private void input(int temperature) {
+    private double fluctuation = 0.5;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner scanner = new Scanner(System.in);
+         while(true) {
+                 if(scanner.hasNextLine()) {
+                     try {
+                         String s = scanner.nextLine();
+                         Double aDouble = Double.valueOf(s);
+                         System.out.println(aDouble);
+
+                         solution.input(aDouble);
+                         solution.alert();
+                     } catch (NumberFormatException nfe) {
+                         System.err.println(nfe);
+                     }
+                 }
+         }
+    }
+
+    private  void input(double temperature) {
         this.priviousTemperature = this.currentTemperature;
         this.currentTemperature = temperature;
     }
 
-    private String alert(int temperature) {
-          if(temperature <= freezing) {
-              return "freezing";
+    /**
+     * +警报由方向定义。
+     * 如果之前的温度高于冰点，则会触发警报“冻结”，如果之前的温度低于冰点，则会触发警报“解冻”。
+     * 类似于“沸腾”和“未沸腾”。
+     */
+    private  void alert() {
+//          if(this.currentTemperature <= freezingThresholds) {
+//              System.out.println(FREEZING);
+//          }
+
+          if(this.priviousTemperature < boilingThresholds &&
+         this.currentTemperature >= boilingThresholds) {
+              System.out.println(BOILING);
           }
 
-          if(temperature >= boiling) {
-              return "boiling";
+        if(this.priviousTemperature > boilingThresholds &&
+                this.currentTemperature <= boilingThresholds) {
+            System.out.println(UNBOILING);
+        }
+
+          if(this.priviousTemperature > freezingThresholds &&
+                  this.currentTemperature <= freezingThresholds) {
+              System.out.println(FREEZING);
           }
 
-          return "";
+          if(this.priviousTemperature < freezingThresholds &&
+                  this.currentTemperature >= freezingThresholds) {
+                System.out.println(UNFREEZING);
+          }
     }
 
 
